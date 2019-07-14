d19c9f61c66e0f8d05db8c1ec0f7b1e48c72e22e
Yucong Jin <yucongj@microsoft.com>
2019-07-14 17:30:07 +0800

    intstall mxgraph

--- src/components/MxGraph.tsx
+++ src/components/MxGraph.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 
+// @ts-ignore
+import * as mxGraph from "mxgraph-js";
+
+console.log(mxGraph);
+
 export class MxGraph extends React.PureComponent {
   public render(): React.ReactNode {
     return this.props.children;
