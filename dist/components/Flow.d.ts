39b2c873be5ebc23158efe1ef2638545ad01f7dc
Yucong Jin <yucongj@microsoft.com>
2019-07-14 19:49:51 +0800

    remove dist

--- dist/components/Flow.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { ICanvasData } from "../types/flow";
-interface IFlowProps {
-    data: ICanvasData;
-}
-export declare class Flow extends React.PureComponent<IFlowProps> {
-    render(): React.ReactNode;
-}
-export {};
--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./components/MxGraph";
-export * from "./components/Flow";
