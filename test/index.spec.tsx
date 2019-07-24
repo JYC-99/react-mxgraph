6cf1aaa5c18c47a1493b70a4818e6888949968e5
Yucong Jin <yucongj@microsoft.com>
2019-07-24 20:50:37 +0800

    strcuture test passed

--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -1,23 +1,3 @@
-import "jest";
-import * as React from "react";
-import { shallow } from "enzyme";
+import initStoryshots from '@storybook/addon-storyshots';
 
-import { Example, SfcExample } from "../src/index";
-
-describe("Example", () => {
-  it("renders correctly", () => {
-    const wrapper = shallow(
-      <Example />,
-    );
-    expect(wrapper).toMatchSnapshot();
-  });
-});
-
-describe("SfcExample", () => {
-  it("renders correctly", () => {
-    const wrapper = shallow(
-      <SfcExample />,
-    );
-    expect(wrapper).toMatchSnapshot();
-  });
-});
+initStoryshots({ /* configuration options */ });
